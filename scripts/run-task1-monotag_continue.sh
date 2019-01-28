8e74eb047fc6a599c1defa3f44a8f44101eed7f6
Adamits <ajwieme@gmail.com>
2019-01-28 15:15:13 -0500

    running defaults

--- scripts/run-task1-monotag_continue.sh
+++ scripts/run-task1-monotag_continue.sh
@@ -8,6 +8,7 @@ python3.7 src/train.py \
     --train sample/task1/$pair/$highreslang-train-high sample/task1/$pair/$lowreslang-train-low  \
     --dev sample/task1/$pair/$lowreslang-dev \
     --model model/sigmorphon19/task1/monotag-$arch/$pair --seed 0 \
+    --load ~/crosslingual-inflection-baseline/model/sigmorphon19/task1/monotag-hmm/adyghe--kabardian.nll_0.2800.acc_32.0.dist_2.64.epoch_2 \
     --embed_dim 200 --src_hs 400 --trg_hs 400 --dropout 0.4 \
     --src_layer 2 --trg_layer 1 --max_norm 5 \
     --arch $arch --estop 1e-8 --epochs 50 --bs 20 --mono
